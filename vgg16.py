a71912d6b0620831378f4afab170b12b3f47d38c
Adilijiang Ainihaer <anharadiljan@gmail.com>
2017-03-22 15:45:40 -0700

    implemented residual network

--- vgg16.py
+++ vgg16.py
@@ -14,8 +14,9 @@ import numpy as np
 from scipy.misc import imread, imresize
 
 class vgg16:
-	def __init__(self, img, weights=None, sess=None):
-		self.img = img
+	def __init__(self, imgs, weights=None, sess=None):
+		self.imgs = imgs
+		self.sess = sess
 		self.conv_layers()
 		#if weights is not None and sess is not None:
 			#self.load_weights(weights, sess)
@@ -27,7 +28,7 @@ class vgg16:
 		with tf.name_scope('preprocess') as scope:
 			# This mean value is calculated over the entire ImageNet dataset
 			mean = tf.constant([123.68,116.779,103.939], dtype=tf.float32, shape=[1,1,1,3], name='img_mean')
-			images = self.img - mean
+			images = self.imgs - mean
 		
 		# conv1_1, 3x3 kernel, stride=[1,1,1,1], output 224x224x64
 		with tf.name_scope('conv1_1') as scope:
@@ -127,22 +128,31 @@ class vgg16:
 			out = tf.nn.bias_add(conv, biases)
 			self.conv4_3 = tf.nn.relu(out, name=scope)
 			self.parameters += [kernel, biases]
+			# perform batch normalization on self.conv4_3, 1x1 convolution, then upscale
+			#kernel = tf.Variable(tf.truncated_normal([1,1,512,256], dtype=tf.float32, stddev=1e-1))
+			#upconv4_3 = tf.nn.conv2d(normalize(self.conv4_3), kernel, [1,1,1,1], padding='SAME')
+			#upconv4_3 = tf.image.resize_bilinear(upconv4_3, 
 		
 		# get hypercolumns: upscale and concatenate, output 224x224x963
-		upconv2_2 = tf.image.resize_bilinear(self.conv2_2, (224,224))
-		upconv3_3 = tf.image.resize_bilinear(self.conv3_3, (224,224))
-		upconv4_3 = tf.image.resize_bilinear(self.conv4_3, (224,224))
-		self.hypercolumns = tf.concat(3, [images, self.conv1_2, upconv2_2, upconv3_3, upconv4_3])
+		#upconv2_2 = tf.image.resize_bilinear(self.conv2_2, (224,224))
+		#upconv3_3 = tf.image.resize_bilinear(self.conv3_3, (224,224))
+		#upconv4_3 = tf.image.resize_bilinear(self.conv4_3, (224,224))
+		#self.hypercolumns = tf.concat(3, [images, self.conv1_2, upconv2_2, upconv3_3, upconv4_3])
 		#print tf.shape(self.hypercolumns)
+	'''
+	def normalize(self, x):
+		mean, variance = tf.nn.moments(x, [0,1,2])
+		return tf.nn.batch_norm_with_global_normalization(x, mean, variance)
+	'''
 
-
-img = tf.placeholder(tf.float32, [None, 224, 224, 3])
-vgg = vgg16(img)
+'''
+imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
+vgg = vgg16(imgs)
 img1 = imread('laska.png', mode='RGB')
 img1 = imresize(img1, (224, 224))
 
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	#hp = sess.run(vgg.hypercolumns, feed_dict={vgg.img: [img1]})
-	hp = sess.run(vgg.hypercolumns, feed_dict={vgg.img: [img1]})
+	hp = sess.run(vgg.hypercolumns, feed_dict={vgg.imgs: [img1]})
 	print hp.shape
+'''
